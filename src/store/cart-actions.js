1c60f97aeed79dad59c547c6bfea091806bf2ef2
Mustafa Ihsan Kabakcili <mustafaihsankabakcili@gmail.com>
2022-07-03 15:16:39 +0200

    first commit

---
+++ src/store/cart-actions.js
@@ -0,0 +1,79 @@
+import { uiActions } from "./ui-slice";
+import { cartActions } from "./cart-slice";
+
+export const fetchCartData = () => {
+  return async (dispatch) => {
+    const fetchData = async () => {
+      const res = await fetch(
+        "https://project-19-ab174-default-rtdb.europe-west1.firebasedatabase.app/cart.json"
+      );
+
+      if (!res.ok) {
+        throw new Error("Could not fetch data!");
+      }
+
+      const data = await res.json();
+
+      return data;
+    };
+
+    try {
+      const cartData = await fetchData();
+      dispatch(cartActions.replaceCart({items: cartData.items || [], totalQuantity: cartData.totalQuantity}));
+    } catch (err) {
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Error!",
+          message: "Sent cart data failed!",
+        })
+      );
+    }
+  };
+};
+
+export const sendCartData = (cart) => {
+  return async (dispatch) => {
+    dispatch(
+      uiActions.showNotification({
+        status: "pending",
+        title: "Sending...",
+        message: "Sending cart data!",
+      })
+    );
+
+    const sendRequest = async () => {
+      const res = await fetch(
+        "https://project-19-ab174-default-rtdb.europe-west1.firebasedatabase.app/cart.json",
+        {
+          method: "PUT",
+          body: JSON.stringify({items: cart.items, totalQuantity: cart.totalQuantity}),
+        }
+      );
+
+      if (!res.ok) {
+        throw new Error("Sending cart data failed!");
+      }
+    };
+
+    try {
+      await sendRequest();
+
+      dispatch(
+        uiActions.showNotification({
+          status: "success",
+          title: "Success!",
+          message: "Sent cart data successfully!",
+        })
+      );
+    } catch (error) {
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Error!",
+          message: "Sent cart data failed!",
+        })
+      );
+    }
+  };
+};
